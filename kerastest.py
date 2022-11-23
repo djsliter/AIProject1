f3ae56ffd3bfe4b060ca4106f1079dace63dcb0d
Dakota Sliter <djsliter@gmail.com>
2022-11-23 15:25:17 -0500

    Testing out keras for NN

---
+++ kerastest.py
@@ -0,0 +1,28 @@
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
+# Create the neural network model
+model = Sequential()
+# Add layers to the model
+layer_a = model.add(Dense(3, input_shape=(3, ), activation="relu"))
+
+out_layer = model.add(Dense(1, activation="relu"))
+model.set_weights([
+    np.array([[0.3, 0.1, 0.4], [0.3, 0.1, 0.4], [0.3, 0.1, 0.4]], dtype=np.float32), 
+    np.array([0., 0., 0.], dtype=np.float32), 
+    np.array([[0.3], [0.1], [0.4]], dtype=np.float32), 
+    np.array([0.], dtype=np.float32)
+])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+data = [[0.1, 0.4, 0.5],[0.4, 0.3, 0.6],[0.2, 0.4, 0.4]]
+model.evaluate(data,  [[1], [1], [1]])
+
+print(model.get_weights())
+
+# 1        1
+# 1        1       1
+# 1        1   
--- test.py
+++ test.py
@@ -1,7 +1,43 @@
 import numpy as np
 import neurolab as nl
 
-input = np.random.uniform(-0.5, 0.5, (10, 2))
-target = (input[:, 0] + input[:, 1]).reshape(10, 1)
+# Make neural network, get all weights into list/array
+# Inputs: food position, snake head pos, snake tail pos
+# foodX, foodY, headX, headY, tailX, tailY
+# Outputs: left, right, up, down
+
+input = np.random.uniform(-0.5, 0.5, (10, 3))
+target = (input[:, 0] + input[:, 1] + input[:, 2]).reshape(10, 1)
 # Create network with 2 inputs, 5 neurons in input layer and 1 in output layer
-net = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5]], [5, 1])
+net = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5]], [8, 1])
+# Train process
+net.trainf = nl.train.train_gd
+err_progress = net.train(input, target, epochs= 40000, show=2000, goal=0.001)
+# print(err_progress)
+# Compute average from 100 runs
+i = 0
+avg = 0
+while i < 1000:
+    avg += net.sim([[0.1, 0.4, 0.5]])[0][0] # x + y + z
+    i+= 1
+print("RESULT ------------- ", avg/i)
+
+# i= 0
+# while i < len(net.layers):
+#     print("W", i, ": ", net.layers[i].np['w'])
+#     i+=1
+
+# [0.45, 0.2, 0.35]
+
+# Input        Hidden          Output
+#                 1
+#                 1
+#                 1
+# 0.45            1
+#                 1
+#                 1             1 => 
+# 0.2             1
+#                 1
+# 0.35            1
+#                 1
+#                 1
