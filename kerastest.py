57617822f0b046477d4d0fd2e6f9629ab0d2c2a9
Dakota Sliter <djsliter@gmail.com>
2022-11-23 16:11:57 -0500

    messing with keras

--- kerastest.py
+++ kerastest.py
@@ -7,8 +7,8 @@ import numpy as np
 model = Sequential()
 # Add layers to the model
 layer_a = model.add(Dense(3, input_shape=(3, ), activation="relu"))
-
 out_layer = model.add(Dense(1, activation="relu"))
+
 model.set_weights([
     np.array([[0.3, 0.1, 0.4], [0.3, 0.1, 0.4], [0.3, 0.1, 0.4]], dtype=np.float32), 
     np.array([0., 0., 0.], dtype=np.float32), 
@@ -16,13 +16,18 @@ model.set_weights([
     np.array([0.], dtype=np.float32)
 ])
 
+# weights = model.get_weights()
+# genome = []
+# genome.add(weights[0])
+# genome.add(weights[2])
+# print(genome)
+
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 data = [[0.1, 0.4, 0.5],[0.4, 0.3, 0.6],[0.2, 0.4, 0.4]]
-model.evaluate(data,  [[1], [1], [1]])
+model.evaluate(data, [[1], [1], [1]])
 
-print(model.get_weights())
-
+print(model.summary())
 
 # 1        1
 # 1        1       1
